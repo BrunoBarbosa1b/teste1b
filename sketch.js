36535d255b5d46904a0dd6d035cb08a0ca21ae78
BrunoBarbosa1b <00001086130182SP@al.educacao.sp.gov.br>
2024-08-19 17:09:38 -0300

    colocando os aquivos js e css

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(400, 400);
+  background(100);
+  
+  }
+  
+  function draw() {
+    
+  if(mouseIsPressed){
+    rect(mouseX,mouseY,40,70);
+    
+  }
+    
+    
+  stroke("red")
+  fill("blue")
+  
+  }
